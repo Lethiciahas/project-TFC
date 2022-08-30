d6884e1d4360cc1fb82cecff54e190bcceea7d3c
Lethiciahas <lethiciahas@gmail.com>
2022-08-30 16:37:54 -0400

    fix: correções de lint

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from 'express';
 import MatchesService from '../services/matchesService';
 
 export default class MatchesController {
-    private service: MatchesService;
-  
-    constructor() {
-      this.service = new MatchesService();
-    }
-  
-    public listAll = async (_req: Request, res: Response) => {
-      const matches = await this.service.list();
-      res.status(200).json(matches);
-    };
+  private service: MatchesService;
+
+  constructor() {
+    this.service = new MatchesService();
+  }
+
+  public listAll = async (_req: Request, res: Response) => {
+    const matches = await this.service.list();
+    res.status(200).json(matches);
+  };
 }
--- app/backend/src/services/matchesService.ts
+++ app/backend/src/services/matchesService.ts
@@ -3,19 +3,19 @@ import Teams from '../database/models/TeamsModels';
 import Matches from '../database/models/MatchesModel';
 
 export default class MatchesService {
-    public list = async () => {
-        const matchesList = await Matches.findAll({ include: [
-            { 
-                model: Teams, 
-                as: 'teamHome',
-                attributes: ['teamName']
-            },
-            {
-                model: Teams, 
-                as: 'teamAway',
-                attributes: ['teamName']   
-            }
-        ]});
-        return matchesList;
-    }
+  public list = async () => {
+    const matchesList = await Matches.findAll({ include: [
+      {
+        model: Teams,
+        as: 'teamHome',
+        attributes: ['teamName'],
+      },
+      {
+        model: Teams,
+        as: 'teamAway',
+        attributes: ['teamName'],
+      },
+    ] });
+    return matchesList;
+  };
 }
