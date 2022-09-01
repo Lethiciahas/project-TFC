dde1507f9dd0307cb5ea152416ccdff321a21532
Lethiciahas <lethiciahas@gmail.com>
2022-09-01 14:52:12 -0400

    feat: criado test endpoint /teams

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,37 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import Teams from '../database/models/TeamsModels';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('teams', () => {
+    it('should return status 200', async () => {
+      sinon.stub(Teams, "findAll").resolves([]);
+
+      const response = await chai.request(app)
+      .get('/teams')
+      
+      expect(response.status).to.equal(200);
+
+      sinon.restore();
+  });
+
+    it('should return teams', async () => {
+    sinon.stub(Teams, "findAll").resolves([]);
+    
+    const response = await chai.request(app)
+    .get('/teams')
+    
+    expect(response.body).to.be.deep.equal([]);
+});
+
+});
